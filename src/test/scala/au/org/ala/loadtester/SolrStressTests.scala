0bb52ba1da562c645a68c9b0725d07b264c2b6f3
Peter Ansell <p_ansell@yahoo.com>
2018-05-31 15:19:11 +1000

    Add system property configurable optional properties for solr stress test parameters
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- src/test/scala/au/org/ala/loadtester/SolrStressTests.scala
+++ src/test/scala/au/org/ala/loadtester/SolrStressTests.scala
@@ -42,7 +42,16 @@ class SolrStressTests extends Simulation {
 
   val solrServers = System.getProperty("au.org.ala.loadtester.solr.servers").trim().stripPrefix("\"").stripSuffix("\"").split(" ")
 
-  println(solrServers.mkString(","))
+  val constantUsersPerSecond = System.getProperty("au.org.ala.loadtester.solr.constantuserspersecond", "100").trim().stripPrefix("\"").stripSuffix("\"").toInt
+
+  val peakRequestsPerSecond = System.getProperty("au.org.ala.loadtester.solr.peakrequestspersecond", "100").trim().stripPrefix("\"").stripSuffix("\"").toInt
+
+  val latterRequestsPerSecond = System.getProperty("au.org.ala.loadtester.solr.latterrequestspersecond", "50").trim().stripPrefix("\"").stripSuffix("\"").toInt
+
+  println("Solr servers: " + solrServers.mkString(","))
+  println("Constant users per second: " + constantUsersPerSecond)
+  println("Peak requests per second: " + peakRequestsPerSecond)
+  println("Latter requests per second: " + latterRequestsPerSecond)
 
   // Scala magic incantation ":_*" to convert the array from above to match the varargs method
   val httpProtocol = http
@@ -56,9 +65,9 @@ class SolrStressTests extends Simulation {
   val solrTests = scenario("Users").exec(Search.search)
 
   setUp(
-    solrTests.inject(constantUsersPerSec(100) during (60 minutes))).throttle(
-    reachRps(100) in (45 minutes),
-    jumpToRps(50),
+    solrTests.inject(constantUsersPerSec(constantUsersPerSecond) during (60 minutes))).throttle(
+    reachRps(peakRequestsPerSecond) in (45 minutes),
+    jumpToRps(latterRequestsPerSecond),
     holdFor(15 minutes)).maxDuration(60 minutes).protocols(httpProtocol)
 
 }
